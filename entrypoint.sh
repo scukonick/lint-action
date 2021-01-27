5b4135e2b5d787f6106a1cbb5908436ac1fb5e56
Alexey Malov <alexey.malov@lockwood-publishing.com>
2021-01-27 13:05:25 +0000

    testing action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,23 @@
 
 env | grep GITHUB
 
-env
-
 ls $GITHUB_WORKSPACE
+
+cd "$GITHUB_WORKSPACE" || exit 1
+
+echo "Running linter"
+CHANGE_LIST=$(git diff "${GITHUB_BASE_REF}".."${GITHUB_HEAD_REF}" --name-only | grep '.go$')
+
+if [ -z "$CHANGE_LIST" ]; then
+  echo "No changes in .go files, exiting"
+  exit 0;
+fi
+
+DIRS=""
+
+for f in $CHANGE_LIST; do
+  DIRS+="$(dirname "${f}")\n"
+done
+
+UNIQ_DIRS=$(echo -e $DIRS | sort -u)
+golangci-lint run --new-from-rev HEAD --modules-download-mode vendor --fast $UNIQ_DIRS
